e8592aa6c1399734dc5988b02a15f5027beb10a9
oyewosolomon <oyewosolomon@gmail.com>
2025-03-31 18:41:32 +0100

    Updated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,14 @@ export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        serif: ['PT Serif', 'serif'],
+        sans: ['ui-sans-serif', 'system-ui', 'sans-serif'],
+      },
+      fontWeight: {
+        normal: '400',
+        bold: '700',
+      },
       animation: {
         'fade-in': 'fadeIn 1s ease-in-out',
         'fade-in-up': 'fadeInUp 1s ease-in-out',
